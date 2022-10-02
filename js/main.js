36b116833ddc561bd62cbaadff18524e644af273
Marcelo Nascimento <nascimentolds@hotmail.com>
2022-10-02 02:47:37 -0300

    week 3

--- js/main.js
+++ js/main.js
@@ -2,6 +2,14 @@ const links = [{
     label: "Week 1",
     url: "week1/index.html"
 },
+{
+    label: "Week 2",
+    url: "week2/index.html"
+},
+{
+    label: "Week 3",
+    url: "week3/index.html"
+},
 ]
 
 function generateContents() {
